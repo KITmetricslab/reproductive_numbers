9fee8943b864d5e8c6f8e2d326a11e79dcc66371
AnnaKlesen <84974308+AnnaKlesen@users.noreply.github.com>
2021-06-05 08:13:19 +0200

    new data rki

--- code/csv-Konvertierung-RKI.py
+++ code/csv-Konvertierung-RKI.py
@@ -13,7 +13,7 @@ import requests
 import io
 
 #Datum eingeben
-datum= '2021-06-04'
+datum= '2021-06-05'
 
 #CSV-Datei mit den neuen Daten einlesen
 XLSX_RKI = 'https://www.rki.de/DE/Content/InfAZ/N/Neuartiges_Coronavirus/Projekte_RKI/Nowcasting_Zahlen_csv.csv?__blob=publicationFile'
@@ -40,10 +40,10 @@ for row in df_rki.date:
 
 
 #Nicht benötigte Spalten entfernen
-df_rki.drop('SchÃ¤tzer_Neuerkrankungen', axis=1, inplace=True)
+df_rki.drop('Schätzer_Neuerkrankungen', axis=1, inplace=True)
 df_rki.drop('UG_PI_Neuerkrankungen', axis=1, inplace=True)
 df_rki.drop('OG_PI_Neuerkrankungen', axis=1, inplace=True)
-df_rki.drop('SchÃ¤tzer_Neuerkrankungen_ma4', axis=1, inplace=True)
+df_rki.drop('Schätzer_Neuerkrankungen_ma4', axis=1, inplace=True)
 df_rki.drop('UG_PI_Neuerkrankungen_ma4', axis=1, inplace=True)
 df_rki.drop('OG_PI_Neuerkrankungen_ma4', axis=1, inplace=True)
 
@@ -59,20 +59,20 @@ four_day_r.insert(0, 'target', '4 day R', True)
 
 #Spalten in Zeilen transformieren 
 four_day_r_tr=four_day_r.melt(id_vars= ["data_version","target","date"],
-                value_vars= ['SchÃ¤tzer_Reproduktionszahl_R', 
+                value_vars= ['Schätzer_Reproduktionszahl_R', 
                              'UG_PI_Reproduktionszahl_R',
                              'OG_PI_Reproduktionszahl_R'], 
                 value_name="value")
 
 #Spalte quantil einfügen
 four_day_r_tr.insert(3, 'quantile', '', True)
-four_day_r_tr.loc[four_day_r_tr.variable=='SchÃ¤tzer_Reproduktionszahl_R','quantile']='NA'
+four_day_r_tr.loc[four_day_r_tr.variable=='Schätzer_Reproduktionszahl_R','quantile']='NA'
 four_day_r_tr.loc[four_day_r_tr.variable=='UG_PI_Reproduktionszahl_R','quantile']='0.025'
 four_day_r_tr.loc[four_day_r_tr.variable=='OG_PI_Reproduktionszahl_R','quantile']='0.975'
 
 #Spalte type 
 four_day_r_tr.rename(columns= {'variable': 'type'}, inplace=True)
-four_day_r_tr['type'].replace({'SchÃ¤tzer_Reproduktionszahl_R':'point'}, inplace=True)
+four_day_r_tr['type'].replace({'Schätzer_Reproduktionszahl_R':'point'}, inplace=True)
 four_day_r_tr['type'].replace({'UG_PI_Reproduktionszahl_R':'quantile'}, inplace=True)
 four_day_r_tr['type'].replace({'OG_PI_Reproduktionszahl_R':'quantile'}, inplace=True)
 
@@ -86,20 +86,20 @@ seven_day_r.insert(0, 'target', '7 day R', True)
 
 #Spalten in Zeilen transformieren 
 seven_day_r_tr=seven_day_r.melt(id_vars= ["data_version","target","date"],
-                value_vars= ['SchÃ¤tzer_7_Tage_R_Wert', 
+                value_vars= ['Schätzer_7_Tage_R_Wert', 
                              'UG_PI_7_Tage_R_Wert',
                              'OG_PI_7_Tage_R_Wert'], 
                 value_name="value")
 
 #Spalte quantil einfügen
 seven_day_r_tr.insert(3, 'quantile', '', True)
-seven_day_r_tr.loc[seven_day_r_tr.variable=='SchÃ¤tzer_7_Tage_R_Wert','quantile']='NA'
+seven_day_r_tr.loc[seven_day_r_tr.variable=='Schätzer_7_Tage_R_Wert','quantile']='NA'
 seven_day_r_tr.loc[seven_day_r_tr.variable=='UG_PI_7_Tage_R_Wert','quantile']='0.025'
 seven_day_r_tr.loc[seven_day_r_tr.variable=='OG_PI_7_Tage_R_Wert','quantile']='0.975'
 
 #Spalte type 
 seven_day_r_tr.rename(columns= {'variable': 'type'}, inplace=True)
-seven_day_r_tr['type'].replace({'SchÃ¤tzer_7_Tage_R_Wert':'point'}, inplace=True)
+seven_day_r_tr['type'].replace({'Schätzer_7_Tage_R_Wert':'point'}, inplace=True)
 seven_day_r_tr['type'].replace({'UG_PI_7_Tage_R_Wert':'quantile'}, inplace=True)
 seven_day_r_tr['type'].replace({'OG_PI_7_Tage_R_Wert': 'quantile'}, inplace=True)
 
