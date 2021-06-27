a3ebf2c033a33cfe595f16824cd67a1d4c7b02d4
AnnaKlesen <84974308+AnnaKlesen@users.noreply.github.com>
2021-06-27 19:50:41 +0200

    Andreas Wenzel 7 day data

--- code/csv-aw-Konvertierung.py
+++ code/csv-aw-Konvertierung.py
@@ -57,29 +57,37 @@ seven_day_r_tr['type'].replace({'R(t) 7d (PI upper)':'quantile'}, inplace=True)
 
 seven_day_r_tr.insert(2, 'location', 'DE', True)
 
-seven_day_r_tr.to_csv('./data-processed/2021-06-23-AW_7day.csv', index=False)
+#seven_day_r_tr.to_csv('./data-processed/2021-06-23-AW_7day.csv', index=False)
+
+groups = seven_day_r_tr.groupby(seven_day_r_tr.data_version)
+unique_version = df["data_version"].unique()
+
+for i in unique_version:
+    df = groups.get_group(i)
+    df.to_csv('./data-processed/Andreas Wenzel/'+i+'-AW_7day.csv', index=False)
 
 #Woche/Vorwoche R
-wv_r= df.iloc[:,[0,1,5,6,7]]
+#wv_r= df.iloc[:,[0,1,5,6,7]]
 
-wv_r.insert(1, 'target', 'WV R', True)
+#wv_r.insert(1, 'target', 'WV R', True)
 
-wv_r_tr=wv_r.melt(id_vars= ["data_version","target","date"],
-                value_vars= ['Rt_WV', 
-                             'R(t) WV (PI lower)',
-                             'R(t) WV (PI upper)'], 
-                value_name="value")
+#wv_r_tr=wv_r.melt(id_vars= ["data_version","target","date"],
+ #               value_vars= ['Rt_WV', 
+ #                            'R(t) WV (PI lower)',
+ #                            'R(t) WV (PI upper)'], 
+ #               value_name="value")
 
-wv_r_tr.insert(3, 'quantile', '', True)
-wv_r_tr.loc[wv_r_tr.variable=='Rt_WV','quantile']='NA'
-wv_r_tr.loc[wv_r_tr.variable=='R(t) WV (PI lower)','quantile']='0.025'
-wv_r_tr.loc[wv_r_tr.variable=='R(t) WV (PI upper)','quantile']='0.975'
+#wv_r_tr.insert(3, 'quantile', '', True)
+#wv_r_tr.loc[wv_r_tr.variable=='Rt_WV','quantile']='NA'
+#wv_r_tr.loc[wv_r_tr.variable=='R(t) WV (PI lower)','quantile']='0.025'
+#wv_r_tr.loc[wv_r_tr.variable=='R(t) WV (PI upper)','quantile']='0.975'
 
-wv_r_tr.rename(columns= {'variable': 'type'}, inplace=True)
-wv_r_tr['type'].replace({'Rt_WV':'point'}, inplace=True)
-wv_r_tr['type'].replace({'R(t) WV (PI lower)':'quantile'}, inplace=True)
-wv_r_tr['type'].replace({'R(t) WV (PI upper)':'quantile'}, inplace=True)
+#wv_r_tr.rename(columns= {'variable': 'type'}, inplace=True)
+#wv_r_tr['type'].replace({'Rt_WV':'point'}, inplace=True)
+#wv_r_tr['type'].replace({'R(t) WV (PI lower)':'quantile'}, inplace=True)
+#wv_r_tr['type'].replace({'R(t) WV (PI upper)':'quantile'}, inplace=True)
 
-wv_r_tr.insert(2, 'location', 'DE', True)
+##wv_r_tr.insert(2, 'location', 'DE', True)
 
-wv_r_tr.to_csv('./data-processed/2021-06-23-AW_WVday.csv', index=False)
+#wv_r_tr.to_csv('./data-processed/2021-06-23-AW_WVday.csv', index=False)
+
