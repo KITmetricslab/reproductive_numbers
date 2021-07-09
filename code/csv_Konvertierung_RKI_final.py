7756f5cfcb2cd5277d1c23428f252c89d49d84bc
AnnaKlesen <84974308+AnnaKlesen@users.noreply.github.com>
2021-07-09 11:56:51 +0200

    changes in column type

---
+++ code/csv_Konvertierung_RKI_final.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May  4 17:35:37 2021
+
+@author: annaklesen
+"""
+
+#notwendige Importe tätigen 
+import pandas as pd
+import datetime 
+import os
+
+path = "./data-processed/RKI_4day/"
+rki= os.listdir(path)
+rki.remove('.DS_Store')
+rki.sort()
+
+print(rki)
+
+for file in rki:
+    df_rki= pd.read_csv(path+'/'+file, delimiter=",")              
+
+    df_rki['type'].replace({'UG_PI_Reproduktionszahl_R':'quantile'}, inplace=True)
+
+    df_rki.to_csv(path+'/'+file)
