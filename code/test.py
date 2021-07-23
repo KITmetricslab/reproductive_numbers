079d5e79bc72bda8481eaf4d4c97fc64851247d5
AnnaKlesen <84974308+AnnaKlesen@users.noreply.github.com>
2021-07-23 18:57:28 +0200

    column value updated

--- code/test.py
+++ code/test.py
@@ -2,9 +2,9 @@ import pandas as pd
 import datetime 
 import os
 
-path = "./data-processed/RKI_4day/"
+path = "./data-processed/RKI_7day/"
 rki= os.listdir(path)
-rki.remove('.DS_Store')
+#rki.remove('.DS_Store')
 rki.sort()
 
 print(rki)
@@ -12,6 +12,7 @@ print(rki)
 for file in rki:
     df_rki= pd.read_csv(path+'/'+file, delimiter=",")   
 
-    df_rki['value']=df_rki['value'].apply(lambda x: str(x).replace(',','.'))
+    #df_rki['value']=df_rki['value'].apply(lambda x: str(x).replace(',','.'))
+
 
     df_rki.to_csv(path+'/'+file, index=False)
