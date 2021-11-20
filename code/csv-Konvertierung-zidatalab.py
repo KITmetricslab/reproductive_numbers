e39b2408b4a254202ef2b58341767da9724b69e1
ElisabethBrockhaus <elisabeth.brockhaus@outlook.de>
2021-11-20 10:45:36 +0100

    adjust file name in conversion of zidatalab data

--- code/csv-Konvertierung-zidatalab.py
+++ code/csv-Konvertierung-zidatalab.py
@@ -3,23 +3,23 @@
 import pandas as pd
 import os
 
-path = './data-raw/zidatalab/'
-files  = os.listdir(path)
+path = "./data-raw/zidatalab/"
+files = os.listdir(path)
 
 
 for file in files:
     data = pd.read_json(path + file)
-    data_version = data.iloc[len(data)-1,0]
+    data_version = data.iloc[len(data) - 1, 0]
     s = str(data_version)
 
-    data = data.loc[data.name == 'Gesamt']
-    data.insert(0, 'data_version', s[0:10])
-    data.insert(1, 'target', '7 day R')
-    data.insert(3, 'location', 'DE')
-    data.insert(4, 'type', 'point')
-    data.insert(5, 'quantile', 'NA')
-    data = data.rename(columns={'R':'value'})
+    data = data.loc[data.name == "Gesamt"]
+    data.insert(0, "data_version", s[0:10])
+    data.insert(1, "target", "7 day R")
+    data.insert(3, "location", "DE")
+    data.insert(4, "type", "point")
+    data.insert(5, "quantile", "NA")
+    data = data.rename(columns={"R": "value"})
     data = data.iloc[:, :-2]
 
-    file_name = './data-processed/zidatalab/' + s[0:10] + '_zidatalab.csv'
-    data.to_csv(file_name, index = False)
+    file_name = "./data-processed/zidatalab/" + s[0:10] + "-zidatalab.csv"
+    data.to_csv(file_name, index=False)
